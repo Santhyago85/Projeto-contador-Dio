62a52fbd250c1abcca41a1e032086a3038bb87fe
Santhyago85 <santhyagosorentto@gmail.com>
2022-06-28 20:42:41 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,10 +4,18 @@ var currentNumber = 0
 function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    
+    if(currentNumber >= 0){
+	document.getElementById("currentNumber").style.color = "black";
+	}
 }
 
 function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    
+    if(currentNumber < 0){
+	document.getElementById("currentNumber").style.color = "red";
+	}
 }
 
